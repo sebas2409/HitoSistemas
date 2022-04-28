fc7fd3b8c74ceb8344a81ae622adfcf2817f55b2
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2022-04-28 19:04:34 +0200

    six

--- app/src/main/java/com/watermelon/hito_sistemas/AlumnoViewModel.kt
+++ app/src/main/java/com/watermelon/hito_sistemas/AlumnoViewModel.kt
@@ -4,14 +4,13 @@ import androidx.lifecycle.ViewModel
 
 class AlumnoViewModel: ViewModel() {
 
-    val lista: ArrayList<String> = ArrayList()
     val lista2: ArrayList<String> = arrayListOf("Adrian","Portilla","Alamo","Johan","Sanchez","Ivan","Cervecitas","Iker",
     "Sergio","Isla","Astrid","Juan del campo","Angel","Victor","Bryan","Dani","Guti","Villalobos","Sebas","Diego","Mario","Rafa","Otro Valverde","Luquero")
 
 
 
     fun add(nombre: String){
-        lista.add(nombre)
+        lista2.add(nombre)
     }
 
     fun grouping(num: Int): List<List<String>> {
